86ad144f3fc76da7974ca90aeb966681e829642d
hyw208 <hyw2082004@yahoo.com>
2022-02-23 10:34:58 -0500

    change client id for pub

--- pub.py
+++ pub.py
@@ -10,7 +10,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.connect("IP_ADDRESS", 1883, 60)
 for i in range(300):
-    d = {'client': 'mrk-1010-mock1', 'temp': 29.78515625+i, 'lumen': 179+i}
+    d = {'client': 'mrk-1010-g1', 'temp': 29.78515625+i, 'lumen': 179+i}
     txt = json.dumps(d)
     client.publish('sensor/json', payload=txt, qos=0, retain=False)
     print(f"sent data", str(d))
