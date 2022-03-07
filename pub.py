4d304b24fc8aecbb79c2e22bb25893575d5efb91
hyw208 <hyw2082004@yahoo.com>
2022-03-06 20:22:05 -0500

    add humidity

--- pub.py
+++ pub.py
@@ -11,7 +11,7 @@ client = mqtt.Client(clientId)
 client.on_connect = on_connect
 client.connect("IP_ADDRESS", 1883, 60)
 for i in range(300):
-    d = {'client': 'mrk-1010-g1', 'temp': 29.78515625+i, 'lumen': 179+i}
+    d = {'client': 'mrk-1010-g1', 'temp': 29.78515625+i, 'humidity': 33.93259048+i, 'lumen': 179+i}
     txt = json.dumps(d)
     client.publish('sensor/json', payload=txt, qos=0, retain=False)
     print(f"sent data", str(d))
